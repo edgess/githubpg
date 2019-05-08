7317e8333084ce5928d5fb776047104ae908febd
desktop <edge2@126.com>
2019-05-08 15:30:07 +0800

    test ehcache

--- jiajia/src/main/java/com/jiajia/App.java
+++ jiajia/src/main/java/com/jiajia/App.java
@@ -5,6 +5,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
 import com.jiajia.entity.Oil;
 import com.jiajia.server.OilService;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -30,6 +33,24 @@ public class App {
         JSONObject jsob = new JSONObject();
         String fstring = jsob.toJSONString(oils.get(0));
         System.out.println("fastjson string----" + fstring);
+
+
+        // 1. 创建缓存管理器
+        CacheManager cacheManager = CacheManager.create("C:\\Users\\Admini\\IdeaProjects\\pg\\jiajia\\src\\main\\resources\\ehcache.xml");
+
+        // 2. 获取缓存对象
+        Cache cache = cacheManager.getCache("HelloWorldCache");
+
+        // 3. 创建元素
+        Element element = new Element("key1", oils2);
+
+        // 4. 将元素添加到缓存
+        cache.put(element);
+
+        // 5. 获取缓存
+        Element value = cache.get("key1");
+        System.out.println("ehcache=="+value);
+        System.out.println("Gson ech----"+value.getObjectValue());
 
     }
 }
