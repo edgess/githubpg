a91388f0bf7749e83b5ed9853683b29d25f9e665
desktop <edge2@126.com>
2019-06-10 16:50:59 +0800

    19061004

--- fileserver/src/main/java/com/example/fileserver/StartLoader.java
+++ fileserver/src/main/java/com/example/fileserver/StartLoader.java
@@ -9,11 +9,11 @@ public class StartLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("======================启动参数配置说明======================");
-        System.out.println("java -jar fileserver.jar --fileserver.mode=SYS (默认D:\\smb)");
+        System.out.println("==================Parameter Configuration==================");
+        System.out.println("java -jar fileserver.jar --fileserver.mode=SYS (D:\\smb) def");
         System.out.println("java -jar fileserver.jar --fileserver.mode=SMB (SMB:20.8)");
         System.out.println("java -jar fileserver.jar --fileserver.mode=FTP (FTP:10.30)");
-        System.out.println("======================接口文档说明地址======================");
+        System.out.println("===============Interface Document Description===============");
         System.out.println("http://127.0.0.1:9090/swagger-ui.html");
         System.out.println("===========================================================");
     }
