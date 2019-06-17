8391012ec53af7550db9cfa67fc342df8fc2946e
desktop <edge2@126.com>
2019-06-17 16:32:16 +0800

    19061701

--- demo1/src/test/java/com/example/demo1/testrate.java
+++ demo1/src/test/java/com/example/demo1/testrate.java
@@ -1,6 +1,10 @@
 package com.example.demo1;
 
-import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author edge
@@ -32,5 +36,39 @@ public class testrate {
             sumcash = sumcash + cash * ratelist.get(i);
             System.out.println(sumcash);
         }
+
+        System.out.println(getdaysub("2018-10-25", "2018-10-30"));
+        System.out.println(getday("2018-10-25", -3));
+    }
+
+    public static Date getday(String d1, long day) {
+        long add = day * 24 * 60 * 60 * 1000;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date1 = new Date();
+        try {
+            date1 = sdf.parse(d1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long daya = date1.getTime() + add;
+        return new Date(daya);
+    }
+
+    public static long getdaysub(String d1, String d2) {
+        String dateStr = "2018-10-29";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date1 = new Date();
+        Date date2 = new Date();
+        try {
+            date1 = sdf.parse(d1);
+            date2 = sdf.parse(d2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(date1.getTime());
+        System.out.println(date2.getTime());
+        long add = 24 * 60 * 60 * 1000;
+        long daya = date2.getTime() - date1.getTime();
+        return daya / add;
     }
 }
